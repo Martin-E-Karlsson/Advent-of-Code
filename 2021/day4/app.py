35d9940da17714c862e72d8f6da9e198a7bebadd
Martin Karlsson <m.karlsson91@gmail.com>
2023-01-06 18:08:24 +0100

    Completion of the 1st part of Day 4 2021

--- 2021/day4/app.py
+++ 2021/day4/app.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-possibleBingoRows = [[]]
-
 def read_txt_file(filename):
     with open(filename) as f:
         lines = f.readlines()
@@ -34,20 +32,46 @@ def check_bingo_numbers(bingoNumbers, bingoBoards):
             for y, row in enumerate(board):
                 for x, boardNumber in enumerate(row):
                     if bingoNumber == boardNumber:
-                        # print(bingoNumber, boardNumber, boardIndex)
                         checkBoards[boardIndex][y][x] = 1
         bingoFound, winningBoardIndex = check_for_bingo(checkBoards)
         if bingoFound:
-            break
-    print(checkBoards[0])
-    return winningBoardIndex
+            print("Winning check board")
+            print_matrix(checkBoards[winningBoardIndex])
+            print("Winning bingo board")
+            print_matrix(bingoBoards[winningBoardIndex])
+            return bingoBoards[winningBoardIndex], checkBoards[winningBoardIndex], bingoNumber
 
 def check_for_bingo(checkBoards):
     for boardIndex, board in enumerate(checkBoards):
-        pass
-    return 0, 0
+        #Check rows
+        for row in board:
+            if sum(row) >= 5:
+                return True, boardIndex
+        #Check columns
+        columnSumList = [sum(x) for x in zip(*board)]
+        for columnSum in columnSumList:
+            if columnSum >= 5:
+                return True, boardIndex
+        #Check diagonals
+        mainDiagonalSum = sum([board[i][i] for i in range(5)])
+        antiDiagonalSum = sum([board[4-i][0+i] for i in range(5)])
+        if mainDiagonalSum >= 5 or antiDiagonalSum >= 5:
+            return True, boardIndex
+    return False, boardIndex
 
 
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+
+def get_unmarked_sum(bingoBoard, checkBoard):
+    boardSum = 0
+    for x in range(5):
+        for y in range(5):
+            if checkBoard[x][y] == 0:
+                boardSum += bingoBoard[x][y]
+    return boardSum
 
 
 if __name__=="__main__":
@@ -56,4 +80,6 @@ if __name__=="__main__":
     bingoNumbers, bingoBoards = separate_bingo_data(bingoData)
     # print(bingoNumbers)
     # print(len(bingoBoards))
-    winningBoardIndex = check_bingo_numbers(bingoNumbers, bingoBoards)
+    winningBoard, checkBoard, lastCalledNumber = check_bingo_numbers(bingoNumbers, bingoBoards)
+    unmarkedSum = get_unmarked_sum(winningBoard, checkBoard)
+    print("Final Score:", unmarkedSum * lastCalledNumber)
